0399dd3b40ec2cbc1becf2b1b9a8c6c6ccb4763b
jeliermann <107553762+jeliermann@users.noreply.github.com>
2023-03-22 09:00:18 +0100

    Update config_decoder.py

--- src/config/config_decoder.py
+++ src/config/config_decoder.py
@@ -4,67 +4,68 @@ from typing import TypeVar, Type
 
 import yaml
 
-T = TypeVar('T')
-U = TypeVar('U')
-
-
+Dest = TypeVar('Dest')
+Value = TypeVar('Value')
+ 
 class ConfigDecoder:
 
+    def __is_not_self(p: Parameter) -> bool:
+        return not p.name == 'self'
+
+    def __handle_simple_type(clazz: Type[Dest], value: Value) -> Dest:
+        if clazz != type(value):
+            raise TypeError(f'value "{value}" is not of the right type.Expects "{clazz}", found "{type(value)}"')
+        return clazz(value)
+    
+    def __is_simple_type(t: type) -> bool:
+        return t == str or t == int or t == float or t == bool
+
+    def __process_dict_elem(param: Parameter, _dict: dict):
+        if param.default == Parameter.empty and param.name not in _dict:
+            raise KeyError(f'Missing mandatory key ({param.name})')
+        # Bug : oublié pour upper lvl (valeurs par défaut KO)
+        elif param.name not in _dict:
+            return param.default
+        else:
+            return ConfigDecoder.__handle(param.annotation, _dict[param.name])
+
+    def __handle_dict(clazz: Type[Dest], value: dict) -> Dest: 
+        constructor = signature(clazz.__init__)
+        params = [ConfigDecoder.__process_dict_elem(param, value) 
+                  for param in constructor.parameters.values() 
+                  if ConfigDecoder.__is_not_self(param)]
+        return clazz(*params)
+
+    def __is_list(clazz: type) -> bool:
+        try:    
+            return issubclass(clazz.__origin__, Sequence)
+        except:
+            return False
+        
+    # Bug : ne tient pas compte du type paramétré de la liste
+    def __handle_list(clazz: Type[List[Dest]], value: list) -> List[Dest]:
+            return [ConfigDecoder.__handle(clazz.__args__[0], el) for el in value]
+   
+    def __handle(clazz: Type[Dest], value: Value) -> Dest:
+        if ConfigDecoder.__is_simple_type(clazz):
+            return ConfigDecoder.__handle_simple_type(clazz, value)
+        elif ConfigDecoder.__is_list(clazz):
+            return ConfigDecoder.__handle_list(clazz, value)
+        else:
+            return ConfigDecoder.__handle_dict(clazz, value)
+            
     @staticmethod
-    def __decode(clazz: Type[T]) -> T:
+    def decode(clazz: Type[Dest]):
+        def curried(value: Value) -> Dest:
+            return ConfigDecoder.__handle(clazz, value)
+        return curried
 
-        def is_simple_type(t: type) -> bool:
-            return t == str or t == int or t == float or t == bool
-
-        def is_not_self(p: Parameter) -> bool:
-            return not p.name == 'self'
-
-        def check_same_type(_dict: dict, param: Parameter):
-            if not (type(_dict[param.name]) == param.annotation):
-                raise TypeError(
-                    f'provided value for "{param.name}" ({_dict[param.name]} as {type(_dict[param.name])}) has not the right type {param.annotation}')
-
-        def check_must_exist_in_dict(_dict: dict, param: Parameter):
-            if param.default == Parameter.empty and param.name not in _dict:
-                raise KeyError(f'Missing mandatory key ({param.name})')
-
-        def process(_dict: dict, param: Parameter):
-            if is_simple_type(param.annotation):
-                check_must_exist_in_dict(_dict, param)
-                return process_simple_type(_dict, param)
-            elif type(_dict[param.name]) == list:
-                return process_list(_dict, param)
-            else:
-                check_must_exist_in_dict(_dict, param)
-                return process_object(_dict, param)
-
-        def process_simple_type(_dict: dict, param: Parameter):
-            if (param.name not in _dict) and (not param.default == Parameter.empty):
-                return param.default
-            else:
-                check_same_type(_dict, param)
-                return _dict[param.name]
-
-        def process_list(_dict: dict, param: Parameter):
-            if is_simple_type(param.annotation.__args__[0]):
-                return [elem for elem in _dict[param.name]]
-            else:
-                return [ConfigDecoder.__decode(param.annotation.__args__[0])(elem) for elem in _dict[param.name]]
-
-        def process_object(_dict: dict, param: Parameter):
-            return ConfigDecoder.__decode(param.annotation)(_dict[param.name])
-
-        def _decoder(_dict: dict):
-            constructor = signature(clazz.__init__)
-            params = [process(_dict, param) for param in constructor.parameters.values() if is_not_self(param)]
-            return clazz(*params)
-
-        return _decoder
-
+    # Bug : ne prends pas en charge les dict->class nestées (ex: {"a": 1, "b": {"c": "foo"}})
     @staticmethod
-    def decode_from_json(clazz: Type[U], stream: str | bytes | bytearray) -> U:
-        return json.loads(stream, object_hook=ConfigDecoder.__decode(clazz))
+    def decode_from_json(clazz: Type[Dest], stream: str | bytes | bytearray) -> Dest:
+        return ConfigDecoder.decode(clazz)(json.loads(stream))
 
+    # Pas testé
     @staticmethod
     def decode_from_yaml(clazz: Type[U], stream: str | bytes | bytearray) -> U:
         return ConfigDecoder.__decode(clazz)(yaml.safe_load(stream))
